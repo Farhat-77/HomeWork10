35fa88680fcf624a4d6b09229ed4b0bc2bb0e3a8
Bola_sw25@student.itstep.kz <Bola_sw25@student.itstep.kz>
2021-02-15 10:59:13 +0600

    Добавьте файлы проекта.

---
+++ HomeWork10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HomeWork10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number of Employees; ");
+            int i = 0;
+            int s = int.Parse(Console.ReadLine());
+
+            while (i < s)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("Enter id: ");
+                int _id = int.Parse(Console.ReadLine());
+                Console.WriteLine("");
+                Console.WriteLine("Enter Name: ");
+                string nm = Console.ReadLine();
+                Console.WriteLine("");
+                Console.WriteLine("Enter Salary: ");
+                double sal = int.Parse(Console.ReadLine());
+                i++;
+            }
+        }
+
+    }
+}
+
